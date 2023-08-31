4e074234cdbb91310184611280dc7731aea3168e
Hritik <pypi.pioneer@gmail.com>
2023-08-31 05:42:13 +0530

    notes specification transmuted

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Notes = require('../models/Notes');
+const Tags = require('../models/Tags')
 const fetchuser = require('../middleware/fetchuser');
 const { check, body, validationResult } = require('express-validator');
 
@@ -29,29 +30,56 @@ router.get('/addnotes', fetchuser, [
             return res.status(400).json({ status: 400, message: results["errors"][0]["msg"], where: results["errors"][0]["path"] });
         }
 
-        // if validation succeeded
+        // if validation succeeded, make a note object entries
+        let currentTag = req.body?.tag?.length > 0 ? req.body.tag : "General";
         const notes = new Notes({
             title: req.body.title,
             description: req.body.description,
-            tag: req.body?.tag?.length > 0 ? req.body.tag : "General",
+            tag: currentTag,
             user: req.user.id  // req.user.id is set when fetchuser middleware were called
         });
 
+        // now find the tag in Tags, if not there then only add in db
+        let isTagExist = false;
+        await Tags.findOne({ tag: currentTag }).then((result) => { result && (isTagExist = true) });
+
+        // make a tag object entry
+        if (!isTagExist) {
+            const tag = new Tags({
+                tag: currentTag,
+            });
+
+            // now save the tag in db
+            await tag.save();
+        }
+
         // now save the note
         const savedNotes = await notes.save();
-        res.send(savedNotes);
+        res.json({status: 200, message: 'Notes Added', note: savedNotes});
     } catch (err) {
-        res.status(500).json({ status: 500, message: "Internal server error", issue: err });
+        res.status(500).json({ status: 500, message: "Internal Server Error", issue: err });
     }
 });
 
 // Route 3: To update an existing note by logged in user: "/api/v1/notes/updatenotes/noteid=id" [ using PUT ] (login required)
-router.put('/updatenotes/noteid=:id', fetchuser, async (req, res) => {
+router.put('/updatenotes/noteid=:id', fetchuser, [
+
+    // validating all inputs
+    body('title', "Enter a valid title").isLength({ min: 1, max: 20 }),
+    body('description', "Enter description").isLength({ min: 1, max: 200 }),
+], async (req, res) => {
     try {
+
+        // validating errors for notes updation
+        const results = validationResult(req);
+        if (!results.isEmpty()) {
+            return res.status(400).json({ status: 400, message: results["errors"][0]["msg"], where: results["errors"][0]["path"] });
+        }
+
         const { title, description, tag } = req.body;
 
         // create a new note object
-        const newNote = {};
+        const newNote = {timestamp: Date.now()};
 
         // now, fill all updated details 
         if (title) newNote.title = title;
@@ -61,14 +89,29 @@ router.put('/updatenotes/noteid=:id', fetchuser, async (req, res) => {
         /* find the note that to be updated by logged in user */
         // confirm that the note exist
         let note = await Notes.findById(req.params.id);
-        if (!note) return res.status(404).send("Not Found");
+        if (!note) return res.status(404).json({status: 404, message: "Not Found"});
 
         // confirm the user idenetity
-        if (note.user.toString() !== req.user.id) return res.status(401).send("Not Allowed");
+        if (note.user.toString() !== req.user.id) return res.status(401).json({status: 401, message: "Not Allowed"});
+
+        // now find the tag in Tags, if not there then only add in db
+        const currentTag = tag;  // ?.length > 0 ? tag : "General";
+        let isTagExist = false;
+        await Tags.findOne({ tag: currentTag }).then((result) => { result && (isTagExist = true) });
+
+        // make a tag object entry
+        if (!isTagExist && tag?.length > 0) {  // tag?.length condition will prevent from adding empty and null tags
+            const tag = new Tags({
+                tag: currentTag,
+            });
+
+            // now save the tag in db
+            await tag.save();
+        }
 
         // now, update the note
         note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-        res.json({ note });
+        res.json({status: 200, message: 'Notes Updated', note: note});
     } catch (err) {
         res.status(500).json({ errors: "Internal server error", issue: err });
     }
@@ -81,16 +124,17 @@ router.delete('/deletenotes/noteid=:id', fetchuser, async (req, res) => {
         /* find the note that to be deleted by logged in user */
         // confirm that the note exists
         let note = await Notes.findById(req.params.id);
-        if (!note) return res.status(404).send("Not Found");
+        if (!note) return res.status(404).json({status: 404, message: "Not Found"});
 
         // confirm the user idenetity
-        if (note.user.toString() !== req.user.id) return res.status(401).send("Not Allowed");
+        if (note.user.toString() !== req.user.id) return res.status(401).res.status(401).json({status: 401, message: "Not Allowed"});
 
         // now find the note and delete the note
         note = await Notes.findByIdAndDelete(req.params.id);
-        res.json({ success: "Note has been deleted", note: note, id: req.params.id });
+        res.json({ status: 200, message: "Note has been deleted", note: note });
     } catch (err) {
         res.status(500).json({ errors: "Internal server error", issue: err });
     }
 });
+
 module.exports = router;

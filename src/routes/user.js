7afaa6d09857b7c34479c1a72155e845761770c0
Hritik <pypi.pioneer@gmail.com>
2023-12-04 15:33:23 +0530

    user fixed

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,10 +5,10 @@ const { validateValidationResult } = require('../middlewares/validationMiddlewar
 const { validateRegistrationField, validateLoginFields } = require('../middlewares/validationFields');
 
 
-// Route 1: To create user: '/todo/v1/user/register' [using POST] (login not required)
+// Route 1: To create user: '/api/v1/user/register' [using POST] (login not required)
 router.post('/register', validateRegistrationField, validateValidationResult, registerUser);
 
-// Route 2: To login user: '/todo/v1/user/login' [using POST] (login not required)
+// Route 2: To login user: '/api/v1/user/login' [using POST] (login not required)
 router.post('/login', validateLoginFields, validateValidationResult, loginUser);
 
 // exporting the router object
